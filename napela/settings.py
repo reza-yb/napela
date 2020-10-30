04ee2e221dcf7a1cd13da74aca4b53ee7a673cd4
Reza <ryarbakhsh@gmail.com>
2020-10-30 22:26:14 +0330

    changed database

--- napela/settings.py
+++ napela/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'napela.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
