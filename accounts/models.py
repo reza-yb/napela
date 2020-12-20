ede44e208acaaf299c52b6e508cbc91dfa6f5373
Your Name <you@example.com>
2020-12-20 23:20:32 +0330

    Add local settings and .project file to gitignore

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    university = models.CharField(max_length=100, null=False, blank=True, default='')
+    field = models.CharField(max_length=100, null=False, blank=True, default='')
+    entrance_year = models.IntegerField()
+
